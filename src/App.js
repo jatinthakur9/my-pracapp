8a38aa51b8f27df658d0a540b1549cd402599db1
jatinthakur9 <tjatin29@gmail.com>
2024-10-18 13:24:16 +0530

    task 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import Navbar from "./components/Navbar";
+
+import Home from "./components/Home";
+
+import About from "./components/About";
+import Posts from "./components/Posts";
+
+// // import 4 things
+// import {
+//   BrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// create router
+
+// const routerr = BrowserRouter(
+//   createRoutesFromElements(
+//     <Route>
+//       <Route />
+//     </Route>
+//   )
+// );
+
+import { Routes, Route } from "react-router-dom";
+import Contact from "./components/Contact";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Posts" element={<Posts />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Posts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Posts() {
+  return (
+    <>
+      <div>
+        <h1>My Posts</h1>
+      </div>
+    </>
+  );
+}
+
+export default Posts;
